dc258adf851dc5c648b026c80254b8c5ceefd4b2
Hamed Adefuwa <hamed.adefuwa@gmail.com>
2025-11-08 13:44:08 +0000

    Simplify build script - remove clean and pub get steps

--- build-and-release.bat
+++ build-and-release.bat
@@ -12,15 +12,7 @@ for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set da
 set DATE=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2%
 set TIME=%datetime:~8,2%:%datetime:~10,2%
 
-echo [1/5] Cleaning previous build...
-"%FLUTTER_PATH%" clean
-echo.
-
-echo [2/5] Getting dependencies...
-"%FLUTTER_PATH%" pub get
-echo.
-
-echo [3/5] Building APK (Release)...
+echo [1/3] Building APK (Release)...
 "%FLUTTER_PATH%" build apk --release
 if errorlevel 1 (
     echo ERROR: Build failed!
@@ -29,7 +21,7 @@ if errorlevel 1 (
 )
 echo.
 
-echo [4/5] Copying APK to releases folder...
+echo [2/3] Copying APK to releases folder...
 if not exist "releases" mkdir releases
 copy /Y "build\app\outputs\flutter-apk\app-release.apk" "releases\app-release.apk"
 if errorlevel 1 (
@@ -40,7 +32,7 @@ if errorlevel 1 (
 echo APK copied successfully to releases\app-release.apk
 echo.
 
-echo [5/5] Committing and pushing to GitHub...
+echo [3/3] Committing and pushing to GitHub...
 git add releases\app-release.apk
 git add .
 git commit -m "Update release APK - Build %DATE% %TIME%"
