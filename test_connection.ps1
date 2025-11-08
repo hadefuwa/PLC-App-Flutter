05923140c3ed8083f4212e9f851a2f10f74bd049
Hamed Adefuwa <hamed.adefuwa@gmail.com>
2025-11-08 13:41:44 +0000

    Remove unused PLC server scripts and files

--- test_connection.ps1
+++
@@ -1,8 +0,0 @@
-$body = @{
-    ip = '192.168.0.99'
-    rack = 0
-    slot = 1
-} | ConvertTo-Json
-
-$response = Invoke-RestMethod -Uri http://localhost:5000/api/connect -Method POST -Body $body -ContentType 'application/json'
-$response | ConvertTo-Json -Depth 10
